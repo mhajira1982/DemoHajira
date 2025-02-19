19ad0f7becffe5a3c4defe6c4a3216c3d356cec9
mhajira1982 <33245183+mhajira1982@users.noreply.github.com>
2025-02-19 06:44:04 +0000

    Delete demo directory

--- demo/src/main/java/com/demo/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.demo;
-
-public class HelloWorld {
-public static void main(String args[])
-{
-	System.out.println("Hello & Hii from Cognizant World >>>>> ");
-	System.out.println("Hello2 & Hii2 from Cognizant World 2>>>>> ");
-}
-}
