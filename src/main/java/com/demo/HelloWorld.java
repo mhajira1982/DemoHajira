56d1e5662985a36fe310532ee105090c0998a99c
mhajira1982 <33245183+mhajira1982@users.noreply.github.com>
2025-02-19 06:57:45 +0000

    Update HelloWorld.java

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.demo;
 public class HelloWorld {
 public static void main(String args[])
 {
-	System.out.println("Hello & Hii from Cognizant World >>>>> ");
+	System.out.println("Hello & Hii from Cognizant World 2222>>>>> ");
 }
 }
